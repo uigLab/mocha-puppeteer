161c486d11ffe13adee2ccc1b2cb5f259e5c36c0
UI Galaxy <uigalaxy7@gmail.com>
2020-02-07 21:47:58 +0600

    refactor: code with some fix & remove unused code

--- test/puppeteer.test.js
+++ test/puppeteer.test.js
@@ -1,35 +1,67 @@
+const puppeteer = require("puppeteer");
 const { expect } = require("chai");
-const {
-  facebookLogin,
-  generatePdf,
-  printScreen,
-  getDimensions,
-  websiteScrap
-} = require("../app/puppeteer");
+require("dotenv").config();
 
-describe("#Puppeteer", function() {
-  this.timeout(10000);
+describe("#Puppeteer with Facebook", function() {
+  // Set timeout to infinite
+  this.timeout(0);
 
-  it("should browse and print image", async () => {
-    await printScreen();
-    expect();
-  });
-  it("should browse and print PDF", async () => {
-    await generatePdf();
-    // expect()
-  });
-  it("should browse and log dimentions", async () => {
-    await getDimensions();
-    // expect()
+  let browser;
+  let page;
+  let loggedIn;
+
+  it("Should open browser", async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null
+    });
   });
 
-  it("should scrap page", async () => {
-    const { heading } = await websiteScrap();
-    console.log("Test:", heading);
-    expect(heading).to.eql("< UI Galaxy />");
+  it("Should open new page", async () => {
+    page = await browser.newPage();
   });
 
-  it("should login to facebook", async () => {
-    await facebookLogin()
+  it("Should load facebook login page", async () => {
+    const response = await page.goto("https://www.facebook.com/login/", {
+      timeout: 0,
+      waitUntil: "domcontentloaded"
+    });
+    expect(response.status()).to.equal(200);
+  });
+
+  it("Should generate screenshot of facebook homepage", async () => {
+    await page.screenshot({ path: "facebook-login.png" });
+  });
+
+  it("Should login facebook successfully", async () => {
+    const emailInput = "#email";
+    const passwordInput = "#pass";
+    const submitSelector = "#loginbutton";
+
+    await page.type(emailInput, process.env.FB_EMAIL); // email address for facebook in .env file //
+    await page.type(passwordInput, process.env.FB_PASS); // password for facebook account in .env file
+
+    // await linkSubmit.click();
+    await page.click(submitSelector);
+    await page.waitForNavigation();
+
+    // Test if we logged in properly
+    try {
+      await page.waitForSelector("#bluebarRoot", { timeout: 10000 });
+      loggedIn = true;
+    } catch (err) {
+      loggedIn = false;
+    }
+    expect(loggedIn).to.true;
+  });
+  it("Should take screenshot of facebook timeline", async () => {
+    if (loggedIn) {
+      await page.screenshot({ path: "timeline.png" });
+    }
+    expect(loggedIn).to.true;
+  });
+
+  it("Browser Closes Successfully", async () => {
+    await browser.close();
   });
 });
